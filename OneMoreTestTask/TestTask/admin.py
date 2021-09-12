355919b78238f3519bc522a0d9f24d68ac5c8581
Eelolo <Eelolo@yandex.ru>
2021-09-12 04:09:17 +0300

    provided editing of related records

--- OneMoreTestTask/TestTask/admin.py
+++ OneMoreTestTask/TestTask/admin.py
@@ -2,8 +2,33 @@ from django.contrib import admin
 from .models import Company, Contact, Deal, DealStage, DealStatus
 
 
-admin.site.register(Company)
 admin.site.register(Contact)
-admin.site.register(Deal)
 admin.site.register(DealStage)
 admin.site.register(DealStatus)
+
+
+"""
+The ContactInline and DealStatusInline classes
+provide editing of records of related tables 
+from records of the Company and Deal models
+"""
+
+
+class ContactInline(admin.TabularInline):
+    model = Contact
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('ownership', 'name')
+    inlines = [ContactInline]
+
+
+class DealStatusInline(admin.TabularInline):
+    model = DealStatus
+
+
+@admin.register(Deal)
+class DealAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact')
+    inlines = [DealStatusInline]
