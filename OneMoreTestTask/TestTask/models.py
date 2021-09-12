12b7174a916f0adf9d289f8890da4a073158ac86
Eelolo <Eelolo@yandex.ru>
2021-09-12 03:53:06 +0300

    added Company model

--- OneMoreTestTask/TestTask/models.py
+++ OneMoreTestTask/TestTask/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    """Model that represents a company and its type of ownership"""
+
+    OOO = 'ООО'
+    OAO = 'ОАО'
+    ZAO = 'ЗАО'
+    PAO = 'ПАО'
+
+    OWNERSHIP_TYPES = [
+        (OOO, 'ООО'),
+        (OAO, 'ОАО'),
+        (ZAO, 'ЗАО'),
+        (PAO, 'ПАО'),
+    ]
+
+    ownership = models.CharField(
+        max_length=3, choices=OWNERSHIP_TYPES, default=OOO, verbose_name='Company ownership',
+    )
+    name = models.CharField(max_length=100, verbose_name='Company name')
+
+    def __str__(self):
+        return self.name
