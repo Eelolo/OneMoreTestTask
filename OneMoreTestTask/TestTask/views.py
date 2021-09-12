38082163b4578b48f972c27fa3f1baf80ae09ce3
Eelolo <Eelolo@yandex.ru>
2021-09-12 04:35:15 +0300

    FilterForm rendering

--- OneMoreTestTask/TestTask/views.py
+++ OneMoreTestTask/TestTask/views.py
@@ -1,9 +1,13 @@
+from django.shortcuts import render
 from django.views import View
+from .forms import FilterForm
 
 
 class IndexPageView(View):
     def get(self, request):
-        pass
+        form = FilterForm()
+
+        return render(request, 'index.html', {'form': form})
 
     def post(self, request):
         pass
